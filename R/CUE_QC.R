0e2e3e09e79436ff383f9e51a7aecf9390fefe28
Gang Li <31409321+GangLiTarheel@users.noreply.github.com>
2020-03-29 09:59:07 -0700

    Update CUE_QC
    Update the shinny app.

--- R/CUE_QC.R
+++ R/CUE_QC.R
@@ -1,125 +1,20 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
-library(shiny)
+R version 3.6.1 (2019-07-05) -- "Action of the Toes"
+Copyright (C) 2019 The R Foundation for Statistical Computing
+Platform: x86_64-apple-darwin15.6.0 (64-bit)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
 
-    # Application title
-    titlePanel("Quality Control for CUE"),
+Natural language support but running in an English locale
 
-    fluidRow(column(3, 
-                    radioButtons("radio", 
-                               h3("Tissues"), 
-                               choices = list("Whole Blood (PTSD)" = 1, 
-                                              "Placenta (ELGAN)" = 2),
-                               selected = 1)),
-        
-        
-        
-    ),
-    textOutput("tissueText"),
-    
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("RMSE_Bar",
-                        "RMSE_Sep",
-                        min = 0,
-                        max = 0.1,
-                        value = 0.0025)
-        ),
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("rmsePlot")
-        )
-    ),
-    
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("Accuracy_Bar",
-                        "Accuracy_Sep",
-                        min = 0.9,
-                        max = 1,
-                        value = 0.95)
-        ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("accPlot")
-        )
-    ),
-    
-    textOutput("QCText")
-)
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-    output$tissueText <- renderText({
-        paste("You have selected", input$radio)
-    })
-    
-    output$rmsePlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        if (input$radio==1){
-            load("/Users/GangLi/Dropbox/DNA Methylation Imputation/Code/CUE/PTSD_CpG_Best_method_list.RData")
-            load("/Users/GangLi/Dropbox/DNA Methylation Imputation/Code/CUE/PTSD_CUE_Evaluations.RData")
-        } else{
-            load("/Users/GangLi/Dropbox/DNA Methylation Imputation/Code/CUE/ELGAN_CpG_Best_method_list.RData")
-            load("/Users/GangLi/Dropbox/DNA Methylation Imputation/Code/CUE/ELGAN_CUE_Evaluations.RData")
-        }
-        
-        x    <- mse.min
-        RMSE_Bar <- seq(0, 0.1, length.out = 200 + 1)
-        #bins <- seq(min(x), max(x), length.out = input$RMSE_Bar + 1)
-
-        # draw the histogram with the specified number of bins
-        #hist(x, breaks = bins, col = 'darkgray', border = 'white')
-        plot(sort(x),type="l",#col='cyan',
-             xlab = "Number of Sites",
-             ylab = "RMSE",
-             main = "RMSE Threshold Selection")
-        abline(h=input$RMSE_Bar,col="red")
-        
-    })
-    
-    output$accPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        if (input$radio==1){
-            load("/Users/GangLi/Dropbox/DNA Methylation Imputation/Code/CUE/PTSD_CpG_Best_method_list.RData")
-            load("/Users/GangLi/Dropbox/DNA Methylation Imputation/Code/CUE/PTSD_CUE_Evaluations.RData")
-        } else{
-            load("/Users/GangLi/Dropbox/DNA Methylation Imputation/Code/CUE/ELGAN_CpG_Best_method_list.RData")
-            load("/Users/GangLi/Dropbox/DNA Methylation Imputation/Code/CUE/ELGAN_CUE_Evaluations.RData")
-        }
-        x    <- acc.max
-        RMSE_Bar <- seq(0.9, 1, length.out = 100 + 1)
-        #bins <- seq(min(x), max(x), length.out = input$RMSE_Bar + 1)
-        
-        # draw the histogram with the specified number of bins
-        #hist(x, breaks = bins, col = 'darkgray', border = 'white')
-        plot(sort(x, decreasing = TRUE),type="l",#col='cyan',
-             xlab = "Number of Sites",
-             ylab = "Accuracy",
-             main = "Accuracy Threshold Selection")
-        abline(h=input$Accuracy_Bar,col="red")
-        
-    })
-    
-    output$QCText <- renderText({
-        paste("RMSE threshold:", input$RMSE_Bar, "; \n Accuracy threshold:", input$Accuracy_Bar,".")
-        
-    })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+> 
